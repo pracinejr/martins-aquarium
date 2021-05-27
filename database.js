147104ce5c1bf14b8c3be5f89d12cef4dea67e0b
Peter Racine Jr <peterracinejr@gmail.com>
2021-05-27 11:40:28 -0500

    fix: added module

--- database.js
+++ database.js
@@ -1,9 +1,37 @@
-const fishDatabase = {
+export const fishDatabase = {
   fish: [
       {
           name: "Bart",
-          food: "crustaceans",
-          size: 3
-      }
+          species: "crustaceans",
+          length: 3,
+          location: "Maine, USA",
+          food: "2 pinches",
+          
+      },
+      {
+          name: "Fluffy",
+          species: "Blow Fish",
+          length: 5,
+          location: "Colorado, USA",
+          food: "7 pinches",
+          
+      },
+      {
+          name: "Teddy",
+          species: "Bait Fish",
+          length: 1,
+          location: "Texas, USA",
+          food: "25 pinches",
+          
+      },
+      {
+          name: "Carlos",
+          species: "Bass",
+          length: 12,
+          location: "New Mexico, USA",
+          food: "17 pinches",
+          
+      },
+      
   ]
 }
