cb56e07f8c2295432178b6d2811e7783f0f7905a
Peter Racine Jr <peterracinejr@gmail.com>
2021-05-27 13:29:26 -0500

    changed name of master and edited .js file

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-import {FishList} from './FishList.js'
+import { FishList } from "./FishList.js";
 
-const parentHTMLElement = document.querySelector(".fishModule")
+const parentHTMLElement = document.querySelector(".fishModule");
 
-parentHTMLElement.innerHTML = FishList()
-
-import {FishList} from './FishList.js'
-
-const parentHTMLElement = document.querySelector(".fishModule")
-
-parentHTMLElement.innerHTML = FishList()
+parentHTMLElement.innerHTML = FishList();
